8f5a001caf0011749d253249ba21c51dee007061
rybalchenko.dn <dmitrii.rybalchenko@domru.ru>
2020-08-03 11:54:49 +0500

    add localhost

--- src/components/device/index.js
+++ src/components/device/index.js
@@ -1,9 +1,9 @@
-import React, { useState, useRef, useMemo } from 'react';
+import React, { useState, useRef, useMemo, useEffect, useCallback } from 'react';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
 import { useDrop } from 'react-dnd';
-
+import { setToStorage, getFromStorage } from 'helpers';
 import { Screen } from 'components/screen';
 import { DeviceMenu } from 'components/device-menu';
 import { randomInteger } from 'helpers';
@@ -12,8 +12,27 @@ import './styles.scss';
 
 const ELEM_COUNT = 20;
 const RANDOM_COUNT = 4;
+const mediaWidthTablet = '(min-width: 768px)';
+const MOBILE_KEY = 'mobile_key';
+const TABLET_KEY = 'tablet_key';
 
-const getScreenArray = (arr) => {
+const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    swipe: false,
+};
+
+const getScreenArray = (arr, isTablet) => {
+    const currKey = isTablet ? TABLET_KEY : MOBILE_KEY;
+    const localData = getFromStorage(currKey);
+    console.log("getScreenArray -> localData 1=", localData)
+    
+    if (Array.isArray(localData) && localData.length) return localData;
+    console.log("getScreenArray -> getScreenArray прошел дальше")
     const result = [];
     let tempArr = [];
     arr.forEach(curr => {
@@ -35,17 +54,7 @@ const getRandomApp = (arr) => {
     return arr.slice(randomValue, randomValue + RANDOM_COUNT);
 }
 
-const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    swipe: false,
-};
-
-const swapArray = function (arr, params) {
+const swapArray = (arr, params) => {
     const { dragIndex, dragCard, dropIndex, dropCard } = params;
     const culcDropIndex = dropIndex === null ? arr[dropCard].length : dropIndex;
     const dragElem = arr[dragCard][dragIndex];
@@ -67,15 +76,36 @@ const swapArray = function (arr, params) {
 }
 
 export const Device = () => {
+    console.log("*** render Device -> Device ***")
     const sliderInstance = useRef(null);
-    const [appsArray, setAppsArray] = useState(useMemo(()=>getScreenArray(apps), []));
+    const tabletQuery = window.matchMedia(mediaWidthTablet);
+    const [isTablet, setIsTablet] = useState(tabletQuery.matches);
+    console.log("Device -> isTablet", isTablet)
+    //const memo = useMemo(()=>getScreenArray(apps, isTablet), [isTablet]);
+    //console.log("Device -> memo-=", memo)
+    const [appsArray, setAppsArray] = useState(useMemo(()=>getScreenArray(apps, isTablet), [isTablet]));
+    console.log("Device -> appsArray", appsArray)
     const randomArray = useMemo(()=>getRandomApp(apps), []);
-    console.log("Device -> randomArray=", randomArray)
     let canSlide = true;
     const setCanSlide = (val) => {
         canSlide = val;
     }
+    
+    const handleMediaChange = useCallback((mql) => {
+        if (isTablet !== mql.matches) {
+            setIsTablet(mql.matches);
+            setAppsArray(getScreenArray(apps, isTablet))
+        }
+    }, [isTablet])
 
+    useEffect(()=>{
+        tabletQuery.addListener(handleMediaChange);
+
+        return () => {
+            tabletQuery.removeListener(handleMediaChange);
+        }
+    }, [tabletQuery, handleMediaChange]);
+    
     const changeSlide = (isNext) => {
         const { slickNext, slickPrev } = sliderInstance?.current;
         if (isNext) {
@@ -92,6 +122,7 @@ export const Device = () => {
         if (newArr === appsArray) return null;
         
         setAppsArray(newArr);
+        setToStorage(isTablet ? TABLET_KEY : MOBILE_KEY , newArr);
     }
 
     const [, leftArrow] = useDrop({
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,3 +3,20 @@ export const randomInteger = (min, max) => {
     const rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
 }
+
+export const getFromStorage = key => {
+    try {
+        if (!key) return;
+        let data = localStorage.getItem(key);
+        data = JSON.parse(data);
+        return data;
+    } catch (e) {
+        console.log('Nothing in storage');
+        return null;
+    }
+};
+
+export const setToStorage = (key, value) => {
+    if (!key || !value) return;
+    localStorage.setItem(key, JSON.stringify(value))
+};
